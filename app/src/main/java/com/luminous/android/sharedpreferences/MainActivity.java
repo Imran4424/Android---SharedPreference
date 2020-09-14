ef733d60bafdd1476cab329e81bf23fc734b2b37
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-14 22:41:51 +0600

    Main Activity

--- app/src/main/java/com/luminous/android/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/luminous/android/sharedpreferences/MainActivity.java
@@ -2,8 +2,13 @@ package com.luminous.android.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences("com.luminous.android.sharedpreferences", )
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.luminous.android.sharedpreferences", Context.MODE_PRIVATE);
+
+        sharedPreferences.edit().putString("name", "Imran").apply();
+        String name = sharedPreferences.getString("name", "");
+        Log.i("Saved Name is: ", name);
+
+        ArrayList<String> friends = new ArrayList<>();
+
+        friends.add("Anik");
+        friends.add("Linkon");
+        friends.add("Nayeem");
+        friends.add("Munir");
     }
 }
