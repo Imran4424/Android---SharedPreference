19f71038368c8db862305b8aade79bcbf1cf3acd
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-14 22:06:41 +0600

    Main Activity

--- app/src/main/java/com/luminous/android/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/luminous/android/sharedpreferences/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPreferences = this.getSharedPreferences(com.luminous.android.sharedpreferences, )
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.luminous.android.sharedpreferences", )
     }
 }
